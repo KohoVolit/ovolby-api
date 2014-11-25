79fc3f9e2bdc9ddcf013ea809b881be786f8b63a
Michal Skop <michal.skop@kohovolit.eu>
2014-11-25 18:27:41 +0100

    tab -> spaces

--- wsgi.py
+++ wsgi.py
@@ -17,29 +17,29 @@ sys.path.insert(0, os.path.dirname(__file__))
 from run import create_app
 
 class PathDispatcher(object):
-	"""Middleware routing from the URLs to individual applications
-	respective to the parliament.
-	"""
-	def __init__(self):
-		"""Creates application instances for all parliaments."""
-		self.instances = {}
-#		with open(os.path.join(os.path.dirname(__file__), 'parliaments.json'), 'r') as f:
-#			parliaments = json.load(f)
-#		for parl, conf in parliaments.items():
-#			self.instances[parl] = create_app(parl, conf)
-	    self.instances['ovolby'] = create_app()
+    """Middleware routing from the URLs to individual applications
+    respective to the parliament.
+    """
+    def __init__(self):
+        """Creates application instances for all parliaments."""
+        self.instances = {}
+#        with open(os.path.join(os.path.dirname(__file__), 'parliaments.json'), 'r') as f:
+#            parliaments = json.load(f)
+#        for parl, conf in parliaments.items():
+#            self.instances[parl] = create_app(parl, conf)
+        self.instances['ovolby'] = create_app()
 
-	def __call__(self, environ, start_response):
-		"""Returns application instance respective to the parliament in
-		the URL.
-		"""
-		app = NotFound()
-#		segments = get_path_info(environ).lstrip('/').split('/', 2)
-#		if len(segments) >= 2:
-#			parl = segments[0] + '/' + segments[1]
-#			if parl in self.instances:
-#				app = self.instances[parl]
+    def __call__(self, environ, start_response):
+        """Returns application instance respective to the parliament in
+        the URL.
+        """
+        app = NotFound()
+#        segments = get_path_info(environ).lstrip('/').split('/', 2)
+#        if len(segments) >= 2:
+#            parl = segments[0] + '/' + segments[1]
+#            if parl in self.instances:
+#                app = self.instances[parl]
         app = self.instances['ovolby']
-		return app(environ, start_response)
+        return app(environ, start_response)
 
 application = PathDispatcher()
